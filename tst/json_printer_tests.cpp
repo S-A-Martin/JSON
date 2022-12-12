79af6c23cfd2e58b2e7e7a008dc08f1da8f7a1d0
Steven Martin <S-A-Martin@hotmail.com>
2022-12-12 15:38:15 +0000

    Added support for constructing from and retrieving into std::vectors

--- tst/json_printer_tests.cpp
+++ tst/json_printer_tests.cpp
@@ -42,12 +42,13 @@ TEST(JSONDataValueJSONObject, StreamOverloadShouldPrintCorrectString) {
     JSON::Data data = JSON::Object{ { "Test String", 1 }, { "Test String 2", 5.5 }, { "Test String 3", true } };
     ss << data;
     std::string expected =
-        // clang format off
-        R"({
+        // clang-format off
+R"({
     "Test String": 1,
     "Test String 2": 5.5,
     "Test String 3": true
 })";
+    // clang-format on
     EXPECT_EQ(ss.str(), expected);
 }
 
